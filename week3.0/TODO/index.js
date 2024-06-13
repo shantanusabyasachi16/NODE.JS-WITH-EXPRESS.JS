fa301c9d1ba194679ac280dec68201c6c4baea81
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-06-13 23:42:57 +0530

    todo

--- week3.0/TODO/index.js
+++ week3.0/TODO/index.js
@@ -18,11 +18,13 @@ function removeAtIndex(arr, index) {
   let newArray = [];
   for (let i = 0; i < arr.length; i++) {
     if (i !== index) newArray.push(arr[i]);
-  }
+  }   
+
   return newArray;
 }
 
 app.get('/todos', (req, res) => {
+  
   res.json(todos);
 });
 
