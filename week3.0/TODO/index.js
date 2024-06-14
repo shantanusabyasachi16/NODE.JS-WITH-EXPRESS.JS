ce1a57881b2d494698815c28e02c3742e5c307ec
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-06-14 21:08:17 +0530

    Todo app

--- week3.0/TODO/index.js
+++ week3.0/TODO/index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');//for post request, to capture body in "post" request
 
 const app = express();
-const port = 3000
+const port = 3000 // tom listen http request
 
 app.use(bodyParser.json());
 
 let todos = [];//global variable to stores all datas
 
 
-//The function findIndex searches for an object with a specific id within an array of objects and returns the index of that object.
+//The function findIndex searches for an object with a specific id within an array of objects and returns the index of that object in array
 // If the object is not found, it returns -1
 
 
@@ -24,11 +24,13 @@ function findIndex(arr, id) {
 //The removeAtIndex function creates a new array that is a copy of the input array, 
 //but with the element at the specified index removed.
 function removeAtIndex(arr, index) {
-  let newArray = [];
+
+  let newArray = [];//A new empty array called newArray is created to store the elements that are not being removed.
+
   for (let i = 0; i < arr.length; i++) {
     if (i !== index) newArray.push(arr[i]);
   }
-  return newArray;
+  return newArray; //it will return the new array or the array which is not removed
 }
 
 
@@ -36,14 +38,7 @@ app.get('/todos', (req, res) => { ///This method defines a route handler for GET
   res.json(todos);// it wiil send the json response
 });
 
-app.get('/todos/:id', (req, res) => {  
-  const todoIndex = findIndex(todos, parseInt(req.params.id));
-  if (todoIndex === -1) {
-    res.status(404).send();
-  } else {
-    res.json(todos[todoIndex]);
-  }
-});
+
 
 app.post('/todos', (req, res) => {   //This is the path or endpoint for the route. When a client makes a POST request to this path, the provided callback function will be executed.
   
@@ -65,23 +60,15 @@ app.post('/todos', (req, res) => {   //This is the path or endpoint for the rout
   
 });
 
-app.put('/todos/:id', (req, res) => {
-  const todoIndex = findIndex(todos, parseInt(req.params.id));
-  if (todoIndex === -1) {
-    res.status(404).send();
-  } else {
-    todos[todoIndex].title = req.body.title;
-    todos[todoIndex].description = req.body.description;
-    res.json(todos[todoIndex]);
-  }
-  
-});
+
 
 app.delete('/todos/:id', (req, res) => {
+  //1st find the index (findIndex)we have with the given id (req.params.id)then
   const todoIndex = findIndex(todos, parseInt(req.params.id));
-  if (todoIndex === -1) {
+  if (todoIndex === -1) {  //if it is not present send this
     res.status(404).send();
-  } else {
+  } else { //if it is there then send this
+    //remove the index from todos
     todos = removeAtIndex(todos, todoIndex);
     res.status(200).send();
   }
